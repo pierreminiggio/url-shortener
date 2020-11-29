159d660ac0bf19bd97f74a31289a1b7753c244be
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-11-29 14:29:44 +0100

    empty

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use App\App;
 require __DIR__ . '/../vendor/autoload.php';
 
 $requestUrl = $_SERVER['REQUEST_URI'];
-$queryParameters = $_SERVER['QUERY_STRING'] ? ('?' . $_SERVER['QUERY_STRING']) : null;
+$queryParameters = ! empty($_SERVER['QUERY_STRING']) ? ('?' . $_SERVER['QUERY_STRING']) : null;
 
 (new App())->run(
     $queryParameters
