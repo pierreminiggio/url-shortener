5daf44f5035d34ce031cec0b37a70527954647b4
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-11-29 14:28:25 +0100

    use request uri

--- public/index.php
+++ public/index.php
@@ -4,4 +4,13 @@ use App\App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-(new App())->run($_SERVER['PATH_INFO'] ?? '/', $_SERVER['QUERY_STRING'] ?? null);
+$requestUrl = $_SERVER['REQUEST_URI'];
+$queryParameters = $_SERVER['QUERY_STRING'] ? ('?' . $_SERVER['QUERY_STRING']) : null;
+
+(new App())->run(
+    $queryParameters
+        ? str_replace($queryParameters, '', $requestUrl)
+        : $requestUrl
+    ,
+    $queryParameters
+);
