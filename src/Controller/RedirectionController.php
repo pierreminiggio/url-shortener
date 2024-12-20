51233d167ff9dcbf669b5652e7935ce2eee51b62
Pierre Miniggio <pierre.miniggio@gmail.com>
2024-12-20 18:20:43 +0100

    Check existing domain subdomains

--- src/Controller/RedirectionController.php
+++ src/Controller/RedirectionController.php
@@ -9,6 +9,8 @@ use App\Repository\ManagedLinksRepository;
 use App\Repository\RedirectionRepository;
 use App\Template\ErrorTemplate;
 use App\Template\RedirectionTemplate;
+use DateInterval;
+use DateTime;
 use Exception;
 
 Class RedirectionController
@@ -60,6 +62,96 @@ Class RedirectionController
         
         $urls[] = App::$allUri;
         $urls[] = App::$allurlsjsonUri;
+
+        $cacheFolder = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'cache';
+
+        if (! file_exists($cacheFolder)) {
+            mkdir($cacheFolder);
+        }
+
+        $domainInfoFile = $cacheFolder . DIRECTORY_SEPARATOR . 'domain-infos.txt';
+        $domain = $_SERVER['HTTP_HOST'] ?? null;
+
+        if (! $domain) {
+            throw new Exception('Empty $_SERVER[\'HTTP_HOST\']');
+        }
+
+        beforeCheckFile:
+        if (! file_exists($domainInfoFile)) {
+            $command = 'dig @ns1.' . $domain . ' ' . $domain . ' axfr > ' . $domainInfoFile;
+            shell_exec($command);
+        }
+
+        if (! file_exists($domainInfoFile)) {
+            throw new Exception('Error while creating file');
+        }
+
+        $fileTimestamp = filectime($domainInfoFile);
+        if ($fileTimestamp === false) {
+            throw new Exception('File timestamp error');
+        }
+
+        $datetime = (new DateTime())->setTimestamp($fileTimestamp);
+        if ((new DateTime()) > (clone $datetime)->add(new DateInterval('P3D'))) {
+            unlink($domainInfoFile);
+            goto beforeCheckFile;
+        }
+
+        $domainDot = $domain . '.';
+        $wwwDot = 'www.';
+
+        $handle = fopen($domainInfoFile, 'r');
+
+        $alreadyChecked = [];
+
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                $line = trim(preg_replace('/\s+/', ' ', $line));
+
+                if (str_contains($line, 'IN A') || str_contains($line, 'IN AAAA') || str_contains($line, 'IN CNAME')) {
+                    $explodedLine = explode(' ', $line, 2);
+                    $entry = $explodedLine[0];
+
+                    if (! str_ends_with($entry, $domainDot)) {
+                        continue;
+                    }
+
+                    $entry = substr($entry, 0, - (strlen($domainDot)));
+
+                    if (str_starts_with($entry, $wwwDot)) {
+                        $entry = substr($entry, strlen($wwwDot));
+                    }
+
+                    if ($entry === '') {
+                        continue;
+                    }
+
+                    if ($entry === '*.') {
+                        continue;
+                    }
+
+                    if (str_ends_with($entry, '.')) {
+                        $entry = substr($entry, 0, -1);
+                    }
+
+                    if (in_array($entry, $alreadyChecked)) {
+                        continue;
+                    }
+
+                    $alreadyChecked[] = $entry;
+
+                    if (str_contains($entry, '.')) {
+                        continue;
+                    }
+                    
+                    if (! in_array($entry, $urls)) {
+                        $urls[] = $entry;
+                    }
+                }
+            }
+
+            fclose($handle);
+        }
 
         return json_encode($urls);
     }
